d2ee47add952c3564d7c7bf1934f40a14c57e3c0
Pichistelrooy <picho_xeneize@hotmail.com>
2020-10-25 16:32:07 -0300

    Estructura base, modelos, testeo basico de clases princ. urls basicas, Admin Armado

---
+++ AirUTN/bookings/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'bookings'
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    #url(r'^search/', views.hotel_search_results, name='hotel_search_results'),
+    #url(r'^property/([0-9]+)/$', views.hotel_details, name='hotel_details'),
+    #url(r'^check_availability/', views.check_availability, name='check_availability'),
+    #url(r'^reservation/([0-9]+)/$', views.successful_booking, name='successful_booking')
+]
