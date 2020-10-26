19490de9863fc3f16f7766ac61b053fe517632b2
unknown <julianulisesaga@gmail.com>
2020-10-26 14:35:47 -0300

    index view finished

--- AirUTN/bookings/urls.py
+++ AirUTN/bookings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'bookings'
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     #url(r'^search/', views.hotel_search_results, name='hotel_search_results'),
     #url(r'^property/([0-9]+)/$', views.hotel_details, name='hotel_details'),
     #url(r'^check_availability/', views.check_availability, name='check_availability'),
