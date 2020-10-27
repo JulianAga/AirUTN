67e38628a359407b87312745e1f896def5f94a34
unknown <julianulisesaga@gmail.com>
2020-10-27 13:55:28 -0300

    cities request from database

--- AirUTN/bookings/views.py
+++ AirUTN/bookings/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import City, Property, ReservationDate, Reservation
+
+
 def index(request):
-    return render(request, 'bookings/index.html')
+    cities = City.objects.all()
+    context = {'cities': cities }
+    return render(request, 'bookings/index.html', context)
