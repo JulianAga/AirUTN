e1b1c3184254cbd1980801b3ce0d07d5807a48a9
unknown <julianulisesaga@gmail.com>
2020-10-27 15:26:00 -0300

    added hotel search

--- AirUTN/bookings/views.py
+++ AirUTN/bookings/views.py
@@ -10,3 +10,26 @@ def index(request):
     cities = City.objects.all()
     context = {'cities': cities }
     return render(request, 'bookings/index.html', context)
+
+def hotel_search(request):
+    if request.method == 'GET':
+        properties = Property.objects.all()
+        context = {'properties': properties}
+        return render(request, 'bookings/hotel_search.html', context)
+    if request.method == 'POST':
+        city = request.POST.get('city', False)
+        pax = request.POST.get('pax', False)
+        # date-begin = request.POST.get('depart', False)
+        # dateEnd = request.POST.get('return', False)
+        properties = Property.objects.all()
+
+        if not city.__eq__('None'):
+            properties = properties.filter(city=city)
+
+        if not pax.__eq__('None'):
+            properties = properties.filter(max_pax__gte=pax)
+
+            # properties = properties.filter(ReservationDate=date-begin)
+
+        context = {'properties': properties}
+        return render(request, 'bookings/hotel_search.html', context)
